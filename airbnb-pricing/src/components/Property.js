56632c4b97340eeba87da7862750d10217f02e54
tyler broadway <tbroadway96@gmail.com>
2019-12-22 23:41:24 -0600

    ListItem compnent created & refactored PropertyList

--- airbnb-pricing/src/components/Property.js
+++ airbnb-pricing/src/components/Property.js
@@ -5,17 +5,17 @@
  import React from "react";
 
  const Property = props => {
- 		return (
- 			<div className = "property">
-				<img src = {props.photo} /> 
-				<h2>{props.title}</h2>
-		 		<h2>{props.price}</h2>
- 				<div className = 'property-info'>
-					<h3>{props.address}</h3>
-		 			<p>Bedrooms: {props.beds}</p>
-		 			<p>Bathrooms: {props.baths}</p>
-				</div>
- 			</div>
+ 	return (
+ 		<div className = "property">
+			<img src = {props.photo} /> 
+			<h2>{props.title}</h2>
+		 	<h2>${props.price}</h2>
+ 			<div className = 'property-info'>
+				<h3>{props.address}</h3>
+		 		<p>Bedrooms: {props.beds}</p>
+		 		<p>Bathrooms: {props.baths}</p>
+			</div>
+ 		</div>
  	);
  }
  export default Property; 
